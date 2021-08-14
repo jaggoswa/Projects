7f5e35bb039d20393420fc4bd598a31a33b47296
jaggoswa <jagriti.goswami10@gmail.com>
2021-08-14 13:27:11 +0530

    Initial commit

--- WebScraping/activity/seTimout.js
+++
@@ -1,5 +0,0 @@
-function fn() {
-    console.log("hello");
-}
-// 
-setTimeout(fn,3000);
--- WebScraping/poc/code.js
+++
@@ -1,8 +0,0 @@
-// nodejs
-let libObj = require("./lib");
-console.log("I am code file");
-console.log(libObj)
-// object -> undefined
-console.log(libObj.a);
-console.log(libObj.varname);
-libObj.fnFunction("Hello");
